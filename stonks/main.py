ee2097a89a04d7ee39906650b6b2bf043a3ae2b3
logananks <62402368+logananks@users.noreply.github.com>
2020-08-18 09:51:07 -0500

    Update main.py

--- stonks/main.py
+++ stonks/main.py
@@ -1,9 +1,11 @@
 import models
-from fastapi import FastAPI, Request, Depends
+import yfinance
+from fastapi import FastAPI, Request, Depends, BackgroundTasks
 from fastapi.templating import Jinja2Templates
 from database import SessionLocal, engine
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
+from models import Stock
 
 app = FastAPI()
 
@@ -20,23 +22,67 @@ def get_db():
         yield db
     finally:
         db.close()
-        
 @app.get("/")
-def home(request: Request):
+def home(request: Request, forward_pe = None, dividend_yield = None, ma50 = None, ma200 = None, db: Session = Depends(get_db)):
     """
     displays the stock screener dashboard / homepage
     """
+    stocks = db.query(Stock)
+
+    if forward_pe:
+        stocks = stocks.filter(Stock.forward_pe < forward_pe)
+
+    if dividend_yield:
+        stocks = stocks.filter(Stock.dividend_yield > dividend_yield)
+    
+    if ma50:
+        stocks = stocks.filter(Stock.price > Stock.ma50)
+    
+    if ma200:
+        stocks = stocks.filter(Stock.price > Stock.ma200)
+
     return templates.TemplateResponse("home.html", {
-        "request": request
+        "request": request, 
+        "stocks": stocks, 
+        "dividend_yield": dividend_yield,
+        "forward_pe": forward_pe,
+        "ma200": ma200,
+        "ma50": ma50
     })
 
+def fetch_stock_data(id: int):
+    db = SessionLocal()
+    stock = db.query(Stock).filter(Stock.id == id).first()
+
+    yahoo_data = yfinance.Ticker(stock.symbol)
+
+    stock.ma200 = yahoo_data.info['twoHundredDayAverage']
+    stock.ma50 = yahoo_data.info['fiftyDayAverage']
+    stock.price = yahoo_data.info['previousClose']
+    stock.forward_pe = yahoo_data.info['forwardPE']
+    stock.forward_eps = yahoo_data.info['forwardEps']
+    
+    if yahoo_data.info['dividendYield'] is not None:
+        stock.dividend_yield = yahoo_data.info['dividendYield'] * 100
+
+    db.add(stock)
+    db.commit()
+
 @app.post("/stock")
-def create_stock(stock_request: StockRequest):
+async def create_stock(stock_request: StockRequest, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
     """
     created a stock and stores it in the database
     """
+
+    stock = Stock()
+    stock.symbol = stock_request.symbol
+
+    db.add(stock)
+    db.commit()
+
+    background_tasks.add_task(fetch_stock_data, stock.id)
+
     return {
         "code": "success",
         "message": "stock created"
     }
-
